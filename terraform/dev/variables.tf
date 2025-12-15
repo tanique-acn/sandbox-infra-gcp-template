06569278ad29ced34dd45e20b1c87693f3e0fd9b
Adams <tanique.a.adams@accenture.com>
2025-12-15 16:28:11 -0500

    fixed fmt error in tf code

--- terraform/dev/variables.tf
+++ terraform/dev/variables.tf
@@ -26,9 +26,9 @@ variable "machine_type" {
   description = "Instance Machine Types"
   type        = map(string)
   default = {
-    micro        = "e2-micro"
-    small        = "e2-small"
-    medium       = "e2-medium"
+    micro  = "e2-micro"
+    small  = "e2-small"
+    medium = "e2-medium"
   }
 }
 
@@ -36,10 +36,10 @@ variable "machine_image_type" {
   description = "Instance Machine Image Types"
   type        = map(string)
   default = {
-    debian        = "debian-cloud/debian-12"
-    centos        = "centos-cloud/centos-10"
-    rhel          = "rhel-cloud/debian-10"
-    windows       = "windows-cloud/windows-2022"
+    debian  = "debian-cloud/debian-12"
+    centos  = "centos-cloud/centos-10"
+    rhel    = "rhel-cloud/debian-10"
+    windows = "windows-cloud/windows-2022"
   }
 }
 
@@ -47,10 +47,10 @@ variable "machine_disk_size" {
   description = "Instance Boot Disk Sizes"
   type        = map(number)
   default = {
-    micro        = 10
-    small        = 20
-    medium       = 50
-    large        = 100
+    micro  = 10
+    small  = 20
+    medium = 50
+    large  = 100
   }
 }
 
